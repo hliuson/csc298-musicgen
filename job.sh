2c7b6214508e4b7bd67ab4c1cd7238680179beba
Harry <hliuson@hotmail.com>
2022-11-10 17:48:32 -0500

    gpu

--- job.sh
+++ job.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --partition=gpu-debug --time=00:30:00  --output=./out/run.log --mem=50G
+#SBATCH --partition=gpu-debug --time=00:30:00  --output=./out/run.log --mem=50G --gres=gpu:2
 hostname
 date
 source /software/miniconda3/4.12.0/bin/activate CSC298-final
