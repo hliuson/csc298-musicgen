beccee37e5bc450760ca56e50c7f442a38f4c981
Harry Liuson <hliuson@Hotmail.com>
2022-11-14 21:12:23 -0500

    system tweaks

--- system.py
+++ system.py
@@ -5,7 +5,9 @@ import os
 
 print("Available GPUs: ", torch.cuda.device_count())
 for i in range(torch.cuda.device_count()):
-    print(" - GPU ", i, ": ", torch.cuda.get_device_name(i))
-print("Available CPUs: ", psutil.cpu_count())
-print("Total RAM (GB): ", psutil.virtual_memory().total / 1024 / 1024 / 1024)
-print("Available RAM: ", psutil.virtual_memory().available / 1024 / 1024 / 1024)
+    print(" - GPU ", i, " memory: ", torch.cuda.get_device_properties(i).total_memory / 1024 / 1024 / 1024, "GB")
+#cpus aviailable on this node
+print("Available CPUs: ", psutil.cpu_count(logical=False))
+
+print("Available RAM: ", psutil.virtual_memory().available / 1024 / 1024 / 1024, "GB")
+print("Total RAM: ", psutil.virtual_memory().total / 1024 / 1024 / 1024, "GB")
